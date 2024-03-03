00f16708d3f8d2f50e1d89e7dc897e228588a9f6
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-03-03 18:36:30 +0000

    task 3 base modal

--- models/base_model.py
+++ models/base_model.py
@@ -19,32 +19,28 @@ class BaseModel():
         self.updated_at = datetime.utcnow()
 
     def __str__(self):
-    """
-    A method that print a string representation
-    """
-
-    return "[{}] ({}) {}".format(self.__class__.__name__,
-            self.id, self.__dict__)
+        """A method that print a string representation """
+    
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-    """
-    A method updates the public instance attrib updated_at with
-    the current datetime
-    """
-    
-    self.updated_at = datetime.utcnow()
+        """
+        A method updates the public instance attrib updated_at with
+        the current datetime
+        """
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
-    """
-    A method returns a dict containing all keys/values of
-    __dict__ of the instance
-    """
+        """
+        A method returns a dict containing all keys/values of
+        __dict__ of the instance
+        """
 
-    dict_new = {}
-    for att in self.__dict__:
-        if att in ["created_at", "updated_at"]:
-            dict_new[att] = getattr(self, att).isoformat()
-        else:
-            dict_new[att] = getattr(self, att)
-    dict_new['__class__'] = self.__class__.__name__
-    return (dict_new)
+        dict_new = {}
+        for att in self.__dict__:
+            if att in ["created_at", "updated_at"]:
+                dict_new[att] = getattr(self, att).isoformat()
+            else:
+                dict_new[att] = getattr(self, att)
+        dict_new['__class__'] = self.__class__.__name__
+        return (dict_new)
