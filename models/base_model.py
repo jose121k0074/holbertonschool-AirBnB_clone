1ba263e25f91b398627573bdb7668fd26de3925a
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-03-06 07:19:29 +0000

    base models delete writespace

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,8 @@ class BaseModel():
 
     def __str__(self):
         """A method that print a string representation """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                    self.id, self.__dict__)
 
     def save(self):
         """
